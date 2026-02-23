1ac47102cb24706e85f8060eaa5e916c48006cae
Hnin Wutt Yi <157496396+HninWuttYi13@users.noreply.github.com>
2026-02-23 15:46:57 +0630

    implemented login and generate token

--- Docs/auth.md
+++ Docs/auth.md
@@ -15,5 +15,25 @@
 
 ### Security:
 
-- Password is hashed using bcrypt
+- Passwords are securely hashed using bcrypt before being stored in the database.
 - Password is not returned in response
+- A JWT access token is generated successfully upon on register and login
+- The token includes the user id in payload
+- The token expires 7 days
+### Success Register(201)
+``` {
+  "_id": "699aff1a6fdea9bb89c4d29f",
+  "username": "john",
+  "email": "john@example.com",
+  "token":  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9..."
+}
+```
+### Error Response(400)
+```{
+  "msg": "Email already exists"
+}
+```
+### Register
+POST /api/auth/register
+### Login
+POST /api/auth/login
--- src/routeController/authController.ts
+++ src/routeController/authController.ts
@@ -1,37 +1,62 @@
 import type { Request, Response } from "express";
 import User from "../dataModels/userModel.js";
 import bcrypt from "bcrypt";
+import { generateToken } from "../utils/generateToken.js";
 export const register = async (req: Request, res: Response) => {
   try {
     const { username, email, password } = req.body;
     if (!username || !email || !password) {
-      return res.status(400).json("please fill all requirements");
+      return res.status(400).json({msg: "please fill all requirements"});
     }
     if (password.length < 6) {
-      return res.status(400).json("password must be at least 6 characters");
+      return res.status(400).json({msg: "Password must be at least 6 characters"});
     }
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     if (!emailRegex.test(email)) {
-     return  res.status(400).json("Invalid email. Please try");
+      return res.status(400).json({msg: "Invalid email. Please try"});
     }
-    const existEmail = await User.findOne({ email });
+    const normalizedEmail = email.toLowerCase();
+    const existEmail = await User.findOne({ email: normalizedEmail });
     if (existEmail)
       return res
         .status(400)
-        .json("this email is already exists. Please try with another");
-    const salt = await bcrypt.genSalt(10);
-    const hashPassword = await bcrypt.hash(password, salt);
+        .json({msg: "Email already exists"});
+    const hashPassword = await bcrypt.hash(password, 10);
     const newUser = new User({
       username,
-      email,
+      email: normalizedEmail,
       password: hashPassword,
     });
     await newUser.save();
+    const token = generateToken(newUser._id.toString());
     return res.status(201).json({
       _id: newUser._id,
       username: newUser.username,
-      email: newUser.email
+      email: newUser.email,
+      token
     });
+  } catch (error) {
+    return res.status(500).json({msg: "internal server error"});
+  }
+};
+export const login = async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+  if (!email || !password)
+    return res.status(400).json({msg: "email or password required"});
+  try {
+    const normalizedEmail = email.toLowerCase();
+    const user = await User.findOne({ email: normalizedEmail }).select("+password");
+    if (!user) return res.status(401).json({ msg: "Invalid credentials" });
+    const checkPassword = await bcrypt.compare(password, user.password);
+    if (!checkPassword) return res.status(401).json({msg: "invalid credentials"});
+    const token = generateToken(user._id.toString())
+    return res.status(200).json({
+      _id: user._id,
+      username: normalizedEmail,
+      email: user.email,
+      token,
+    });
+    
   } catch (error) {
     return res.status(500).json("internal server error");
   }
